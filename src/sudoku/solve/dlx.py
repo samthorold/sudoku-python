d4f5a119d75dd296f1c06ae239a633d174ddc504
Sam Thorold <sam.thorold@gmail.com>
2022-04-15 01:11:06 +0100

    Dancing links - linked lists

---
+++ src/sudoku/solve/dlx.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Iterator
+
+
+@dataclass
+class Node:
+    val: int
+    left: Node | None = None
+    right: Node | None = None
+
+
+def insert_before(node: Node, new: Node) -> Node:
+    if node.left:
+        node.left.right = new
+        new.left = node.left
+    new.right = node
+    node.left = new
+    return new
+
+
+def insert_after(node: Node, new: Node) -> Node:
+    if node.right:
+        new.right = node.right
+        new.right.left = new
+    new.left = node
+    node.right = new
+    return new
+
+
+def pairs(nodes: Iterable[Node]) -> Iterator[tuple[Node, Node]]:
+    pair = []
+    for node in nodes:
+        pair.append(node)
+        if len(pair) > 1:
+            yield tuple(pair)
+        if len(pair) >= 2:
+            pair = pair[-1:]
+
+
+def create_list(nodes: Iterable[Node]):
+    for l, r in pairs(nodes):
+        yield insert_after(l, r)
+
+
+def forwards(node: Node) -> Iterator[Node]:
+    yield node
+    while (node := node.right):
+        yield node
+
---
+++ tests/test_dlx.py
@@ -0,0 +1,96 @@
+from sudoku.solve import dlx
+
+
+def test_node():
+    n1 = dlx.Node(1)
+    n2 = dlx.Node(2)
+    n1.right = n2
+    n2.left = n1
+
+
+def test_insert():
+    n1 = dlx.Node(1)
+    n2 = dlx.Node(2)
+    n3 = dlx.Node(3)
+    n4 = dlx.Node(4)
+
+    dlx.insert_before(n4, n1)
+
+    assert n1.left == None
+    assert n1.right == n4
+    assert n4.left == n1
+    assert n4.right == None
+
+    dlx.insert_after(n1, n2)
+
+    assert n1.right == n2
+    assert n2.left == n1
+    assert n2.right == n4
+    assert n4.left == n2
+
+    dlx.insert_before(n4, n3)
+
+    assert n2.right == n3
+    assert n3.left == n2
+    assert n3.right == n4
+    assert n4.left == n3
+    assert n4.right == None
+
+
+def test_forwards():
+    n1 = dlx.Node(1)
+    n2 = dlx.Node(2)
+    n3 = dlx.Node(3)
+    n4 = dlx.Node(4)
+
+    dlx.insert_after(n1, n2)
+    dlx.insert_after(n2, n3)
+    dlx.insert_after(n3, n4)
+
+    it = dlx.forwards(n1)
+    assert [n.val for n in it] == [1, 2, 3, 4]
+
+    it = dlx.forwards(n2)
+    assert [n.val for n in it] == [2, 3, 4]
+
+    dlx.insert_after(n4, n1)
+
+    it = []
+    for i, n in enumerate(dlx.forwards(n1), 1):
+        it.append(n.val)
+        if i >= 10:
+            break
+
+    assert it == [1, 2, 3, 4, 1, 2, 3, 4, 1, 2]
+
+
+def test_pairs():
+    x = [1, 2, 3, 4]
+    prs = list(dlx.pairs(x))
+    assert prs == [(1, 2), (2, 3), (3, 4)]
+    x = [1]
+    prs = list(dlx.pairs(x))
+    assert prs == []
+
+
+def test_create_list():
+    nodes = [dlx.Node(i) for i in range(1, 5)]
+    for l, r in dlx.pairs(nodes):
+        dlx.insert_after(l, r)
+    
+    it = dlx.forwards(nodes[0])
+    assert [n.val for n in it] == [1, 2, 3, 4]
+
+    nodes = [dlx.Node(i) for i in range(1, 5)]
+    nodes += [nodes[0]]
+    for l, r in dlx.pairs(nodes):
+        dlx.insert_after(l, r)
+    
+    it = []
+    for i, n in enumerate(dlx.forwards(nodes[0]), 1):
+        it.append(n.val)
+        if i >= 10:
+            break
+
+    assert it == [1, 2, 3, 4, 1, 2, 3, 4, 1, 2]
+
