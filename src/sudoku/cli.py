100a0a498d1e8d7e74bf0bb65c05ef0af33ec39a
Sam Thorold <sam.thorold@gmail.com>
2022-07-27 16:14:03 +0100

    #4

--- src/sudoku/cli.py
+++ src/sudoku/cli.py
@@ -9,7 +9,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("method")
 parser.add_argument("board_string")
 parser.add_argument("--log_level", default="CRITICAL")
-parser.add_argument("--iterations", default=100000, type=int)
 parser.add_argument("--display", action="store_true")
 
 
@@ -24,7 +23,6 @@ if __name__ == "__main__":
     print(board)
     solved_board = solvers[args.method].solve(
         board=board,
-        iterations=args.iterations,
         display=args.display,
     )
     print(solved_board)
